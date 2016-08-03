e6728aabe2a8f19307cc7aaefb77abfaf7597016
Matt Hamil <j.matthew.hamil@gmail.com>
2016-08-03 13:12:43 -0500

    Loaded 3/3 JSON files. Ready to write functionality that adds to DOM

--- loadjson.js
+++ loadjson.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   let select = document.querySelector('select');
 
+  // Returns a promise of an XHR request
   function loadJSON(url) {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
@@ -21,6 +22,9 @@ $(document).ready(function() {
     });
   }
 
+  // Loads the categories
+  // Calls populateCategories() that then makes
+  // a request to load the product types
   function loadCategories() {
     loadJSON('categories.json')
       .then(response => {
@@ -53,10 +57,13 @@ $(document).ready(function() {
 
   // Filters the product types by the selected category
   // in the <select> box
-  // Returns an array of product types
+  // Returns an object containing product types
   function filterByCategory(products) {
     let select = document.querySelector('select');
     var filteredProducts = {};
+
+    // Filter all of the products down to the selected category
+    // in the <select> tag
     let selectedProducts = products.filter((product) => {
       // Filter by Coffee
       if (select.value === "Coffee") {
@@ -68,34 +75,72 @@ $(document).ready(function() {
       }
     });
 
-    console.log(`${select.value} products:`, selectedProducts);
+    // This should only log Tea product types OR Coffee product types.
+    // It should never log both.
+    console.log(`${select.value} types:`, selectedProducts);
 
+    // Uses the array selectedProducts to create an object like so:
+    // filteredProducts = {
+    //   0: { name: "", description: "", products: [] },
+    //   1: ...
+    // }
     selectedProducts.forEach((productType, index) => {
       filteredProducts[index] = {
+        id: productType.id,
         name: productType.name,
         description: productType.description,
         products: []
       };
     });
 
-    console.log(filteredProducts);
+    // Logs the newly formatted object
+    // Uncomment this line to see its structure
+    // console.log(filteredProducts);
 
+    // Return the object containing the products
+    // filtered by the selected category
     return filteredProducts;
   }
 
   // Loads the products.json file and
-  // appends the appropriate products
-  // to the DOM.
+  // filters the products by type
   function loadProducts(types) {
     loadJSON('products.json')
       .then(response => {
-        console.log(response);
-        filterByType(response.products);
+        var productsObj = filterByType(types, response.products);
+        console.log(products);
+        addProductsToDom(productsObj);
       });
-  };
+  }
 
-  function filterByType(products) {
-  };
+  /**
+   * Adds the correct products to the filtered types object and returns it
+   * @param {Object} types    An object formatted by filterByCategory()
+   * @param {Array}  products An array of every product
+   * @return The filtered-by-category object with added products
+   **/
+  function filterByType(types, products) {
+    products.forEach((product) => {
+      // ONLY Coffee OR Tea can be selected. Never both.
+      for (type in types) {
+        // If that product type is found
+        if (types[type].id === product.type) {
+          // Add that product
+          // console.log(`Pushed ${product.name}`);
+          types[type].products.push(product);
+        }
+      }
+    });
+
+    return types;
+  }
+
+  // Takes the filtered productsObj and appends them to the DOM
+  // Each product type becomes a new row in the DOM
+  // Each row contains products by type
+  function addProductsToDom(productsObj) {
+
+  }
 
   loadCategories();
 
